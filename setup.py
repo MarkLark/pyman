e807a250dac95f5a5192e3a71e5c88a117792ac4
MarkLark <mark.pittaway@mlit.net.au>
2016-12-17 18:15:34 +1100

    Initial setup.py file

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""
+pyman
+-------------
+
+A CLI for managing python projects
+"""
+from setuptools import setup
+
+
+setup(
+    name='pyman',
+    version='0.1.0a1',
+    url='http://www.mlit.net.au/pyman/',
+    license='MIT',
+    author='Mark Pittaway',
+    author_email='mark.pittaway@mlit.net.au',
+    description='A CLI for managing python projects',
+    long_description=__doc__,
+    packages=['pyman'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: User Interfaces',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent'
+    ]
+)
