6675db9a11b460579f4ae2d8e822a9abae2e8fb8
MarkLark <mark.pittaway@mlit.net.au>
2016-12-17 22:17:52 +1100

    Added GitHub commands (providing 'git commit' and 'git commit <files>' commands)

--- manage.py
+++ manage.py
@@ -38,11 +38,35 @@ class Test( pyman.Page ):
         ])
 
 
+class Git( pyman.Page ):
+    def __init__( self ):
+        super( Git, self ).__init__( "GitHub" )
+
+    def init( self ):
+        self.add([
+            pyman.Action.Cmd( "Commit", "git commit" ),
+            GitCommitFile(),
+            pyman.Action.Back()
+        ])
+
+
+class GitCommitFile( pyman.Action.Cmd ):
+    def __init__( self ):
+        super( GitCommitFile, self ).__init__( "Commit File" )
+
+    def run( self ):
+        filename = raw_input( "Filename: " )
+        self.cmd = "git commit %s" % filename
+        super( GitCommitFile, self ).run()
+
+
+
 menu = pyman.Main( "pyman - Manage" )
 menu.add([
     Doc(),
     PyPi(),
     Test(),
+    Git(),
     pyman.Action.Exit()
 ])
 menu.cli()
