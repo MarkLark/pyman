e3fdef06aef0502baeac19287791f48e66beab4e
MarkLark <mark.pittaway@mlit.net.au>
2016-12-18 10:20:15 +1100

    Updated sphinx config file to use 'flask_small' theme, and added a GitHub 'Fork Me' ribbon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,6 +71,7 @@ language = None
 exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
+# Commented out, as flask_small theme already sets this config option
 pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
@@ -82,13 +83,13 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'flask_small'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = dict( github_fork = 'MarkLark/pyman', index_logo = False )
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
