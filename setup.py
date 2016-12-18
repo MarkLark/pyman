2eaf88854186d3b9bbefeda2e356fbb963124cc8
MarkLark <mark.pittaway@mlit.net.au>
2016-12-18 20:49:52 +1100

    Updated setup.py file to include long_description from README.rst file

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
-"""
-pyman
--------------
-
-A CLI for managing python projects
-"""
 from setuptools import setup
+from os import path
+from codecs import open
 
+root_dir = path.abspath(path.dirname(__file__))
+
+with open( path.join( root_dir, 'README.rst' ), encoding = 'utf-8' ) as f:
+    long_description = f.read()
 
 setup(
     name='pyman',
-    version='0.1.1a2',
+    version='0.1.1a3',
     url='https://github.com/MarkLark/pyman',
     license='MIT',
     author='Mark Pittaway',
     author_email='mark.pittaway@mlit.net.au',
     description='A CLI for managing python projects',
-    long_description=__doc__,
+    long_description=long_description,
     packages=['pyman'],
     zip_safe=False,
     include_package_data=True,
