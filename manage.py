019d854185a20c4bc3c20a61e3c7a4108b900b23
MarkLark <mark.pittaway@mlit.net.au>
2016-12-18 12:10:55 +1100

    Updated manage.py to use new inbuilt pages

--- manage.py
+++ manage.py
@@ -2,74 +2,10 @@
 import pyman
 
 
-class PyPi( pyman.Page ):
-    def __init__( self ):
-        super( PyPi, self ).__init__( "PyPi" )
-
-    def init( self ):
-        self.add([
-            pyman.Action.Cmd( "Package Source", "python setup.py sdist" ),
-            pyman.Action.Cmd( "Package Wheel", "python setup.py bdist_wheel" ),
-            pyman.Action.Cmd( "Upload", "twine upload dist/*" ),
-            pyman.Action.Back()
-        ])
-
-
-class Doc( pyman.Page ):
-    def __init__( self ):
-        super( Doc, self ).__init__( "Documentation" )
-
-    def init( self ):
-        self.add([
-            pyman.Action.Cmd( "Generate HTML", "cd docs; make html; cd .." ),
-            pyman.Action.Cmd( "Clean", "cd docs; make clean; cd .." ),
-            pyman.Action.Back()
-        ])
-
-
-class Test( pyman.Page ):
-    def __init__( self ):
-        super( Test, self ).__init__( "Testing" )
-
-    def init( self ):
-        self.add([
-            pyman.Action.Cmd( "Without Stdout", "nosetests -v tests" ),
-            pyman.Action.Cmd( "With Stdout", "nosetests -vs tests" ),
-            pyman.Action.Back()
-        ])
-
-
-class Git( pyman.Page ):
-    def __init__( self ):
-        super( Git, self ).__init__( "GitHub" )
-
-    def init( self ):
-        self.add([
-            pyman.Action.Cmd( "Commit", "git commit" ),
-            GitCommitFile(),
-            pyman.Action.Cmd( "Push", "git push" ),
-            pyman.Action.Cmd( "History", "git log" ),
-            pyman.Action.Back()
-        ])
-
-
-class GitCommitFile( pyman.Action.Cmd ):
-    def __init__( self ):
-        super( GitCommitFile, self ).__init__( "Commit File" )
-
-    def run( self ):
-        filename = raw_input( "Filename: " )
-        self.cmd = "git commit %s" % filename
-        super( GitCommitFile, self ).run()
-
-
-
-menu = pyman.Main( "pyman - Manage" )
-menu.add([
-    Doc(),
-    PyPi(),
-    Test(),
-    Git(),
+pyman.Main( "pyman - Manage", [
+    pyman.Doc(),
+    pyman.PyPi(),
+    pyman.NoseTest(),
+    pyman.Git(),
     pyman.Action.Exit()
-])
-menu.cli()
+]).cli()
