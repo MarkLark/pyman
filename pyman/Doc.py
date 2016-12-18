cf36c082dfa721e563ec69e4d6a05f7d3ea837dd
MarkLark <mark.pittaway@mlit.net.au>
2016-12-18 13:05:48 +1100

    Fixed bug with generating Single-Page HTML (was still generating Multi-Page HTML)

--- pyman/Doc.py
+++ pyman/Doc.py
@@ -8,7 +8,7 @@ class Doc( Page ):
     def init( self ):
         self.add([
             Action.Cmd( "Generate Multi-Page HTML", "cd docs; make html; cd .." ),
-            Action.Cmd( "Generate Single-Page HTML", "cd docs; make html; cd .." ),
+            Action.Cmd( "Generate Single-Page HTML", "cd docs; make singlehtml; cd .." ),
             Action.Cmd( "Clean", "cd docs; make clean; cd .." ),
             Action.Back()
         ])
